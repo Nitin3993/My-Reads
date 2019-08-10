01530e25e1aa83f9986de1c51f8843c522e04571
Nitin3993 <nitinporwal99@gmail.com>
2019-08-10 22:54:20 +0530

    search functionality added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Homepage from './Homepage';
 import {Route} from 'react-router-dom'
 import SearchBooks from './SearchBook';
+import ShowAll from './ShowAll';
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
   state = {
-
+    books: [],
+    searchedBooks: []
   }
-
+  componentDidMount() {
+    BooksAPI.getAll().then((books)=> {
+      this.setState({books})
+    })
+  }
+  searchBooks(query) {
+    BooksAPI.search(query).then((searchedBooks)=>{
+      if(searchedBooks && !searchedBooks.error)
+        this.setState({searchedBooks})
+    })
+  }
   render() {
     return (
       <div className="app">
-        <Route exact path='/' component = {Homepage}/>
-        <Route path='/search' component = {SearchBooks}/>
+        <Route 
+          exact path = '/' 
+          render={()=> (
+            <ShowAll 
+              books={this.state.books}
+            />
+          )} 
+        />
+        <Route 
+          path='/search' 
+          render = {()=> (
+            <SearchBooks 
+              onSearchBooks = {(query)=>{
+                this.searchBooks(query)
+                }
+              }
+              books={this.state.searchedBooks}
+            />
+          )}
+        />
       </div>
     )
   }
--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,22 +1,66 @@
 import React, {Component} from 'react';
-
 import {Link} from 'react-router-dom'
-
 import './App.css'
 
 class SearchBooks extends Component {
-    render() {
+  state = {
+      query: ''
+  }
+  
+  updateQuery = (query)=>{
+    this.setState({query: query.trim()})
+    if(this.props.onSearchBooks)
+      this.props.onSearchBooks(this.state.query)
+  }
+  
+  render() {
+    const {query} = this.state;
+    const {books}= this.props;
         return (
             <div className="search-books">
-            <div className="search-books-bar">
-              <Link to='/' className="close-search" >Close</Link>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
+              <div className="search-books-bar">
+                <Link to='/' className="close-search" >Close</Link>
+                <div className="search-books-input-wrapper">
+                  <input value = {query} 
+                    onChange={(e)=>{
+                      this.updateQuery(e.target.value)
+                    }} 
+                    books= {this.state.books} 
+                    type="text" 
+                    placeholder="Search by title or author"
+                  />
+                </div>
               </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
+              <div className="search-books-results">
+                <ol className="books-grid">
+                  {books &&
+                    books.map((book)=> (
+                      <li key = {book.id}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail}` }}></div>
+                            <div className="book-shelf-changer">
+                              <select value = {book.shelf}>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          { book.author && 
+                            <div className="book-authors">
+                              {book.authors[0]}
+                            </div>
+                          }
+                        </div>
+                      </li>
+                    ))
+                  }
+                </ol>
+              </div>
           </div>
         )
     }
